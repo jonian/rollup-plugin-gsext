612a2057cffb0d78a31bbe3356982d9b314a5df4
jonian <jonian@hardpixel.eu>
2022-05-01 18:31:13 +0300

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import pkg from './package.json'
 
 export default {
   input: 'src/index.js',
-  external: ['fs', 'child_process'],
+  external: ['fs', 'child_process', 'xml-js'],
   output: [
     { file: pkg.main, format: 'cjs', exports: 'auto' },
     { file: pkg.module, format: 'es' }
